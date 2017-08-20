e99475d55975fa2982b94f4408a9fcbf194491cb
Kelwin Lee <kelwin.lee@gmail.com>
2017-08-21 01:15:16 +0800

    Added about me directive, controller and JSON data

--- js/app.js
+++ js/app.js
@@ -47,4 +47,21 @@
       controllerAs: "headerCtrl"
     };
   });
+
+  app.directive("appAboutme", function(){
+    return {
+      restrict: "E",
+      templateUrl: "directives/app-aboutme.html",
+      controller: function(){
+        this.aboutMe = {
+          imageSrc: "images/myphotocrop2.jpg",
+          aboutMeTitle: "Kelwin Lee",
+          aboutMeSubtitle: "Web Application Developer",
+          aboutMeText: "I am a software developer who's mainly working on Java/J2EE web applications development. I graduated from the University of Nottingham in 2013, and have been working in software vendors ever since, which includes banking solutions company and planning solutions company. As of now, I have 3 years and 8 months of working experience.",
+          aboutMeText2: "I consider myself a passionate programmer as I'm very enthusiastic and self-motivated to learn new technologies and improve my programming skills. I am also an open-minded individual who is open to ideas and is willing to learn from criticism. I've also been told that I'm easy to work with. While I'm a team worker, I am also able to work independently in busy environments."
+        };
+      },
+      controllerAs: "aboutMeCtrl"
+    };
+  });
 })();
